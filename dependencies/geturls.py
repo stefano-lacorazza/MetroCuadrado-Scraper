88452e018647894678b0debce7c5c428688e3a32
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-07-17 16:29:40 -0500

    update to Selenuim 4.10.0

--- dependencies/geturls.py
+++ dependencies/geturls.py
@@ -20,7 +20,7 @@ class ScraperUrls():
         self.options.add_argument('--ignore-ssl-errors=yes')
         self.options.add_argument('--ignore-certificate-errors')
         #self.options.add_argument("--user-data-dir={}".format(os.path.join(LOG_FOLDER,'USER_DATA')))
-        self.driver = webdriver.Chrome(options=self.options, executable_path=DRIVER_PATH)
+        self.driver = webdriver.Chrome(options=self.options)
 
     def AddFilter(self):
         input("Press Enter to continue...")
@@ -30,12 +30,12 @@ class ScraperUrls():
         self.driver.get(page)
 
     def GetListingQuantity(self):
-        ListingQuantity = self.driver.find_element_by_xpath("/html/body/div[@id='__next']/div[@class='UtilityProvider-wqw4uj-0 gDcynq']/div[@class='appprovider__AppThemeWrapper-asxde5-0 gWkYZE']/div[@class='Layout__LayoutStyled-sc-9y7jis-0 ibZBWk page-container']/div[@class='Container-u38a83-0 jDuhNh inner-container container']/div[@class='Row-sc-2hg243-0 iUVxfs align-items-center mb-4 row']/div[@class='Col-sc-14ninbu-0 lfGZKA mb-3 mb-sm-0 col-12 col-md-8']/span[@class='Breadcrumb-sc-15mocrt-0 aAjZo Breadcrumb-sc-1df07y0-0 bLMdQH valing-center d-block breadcrumb']/h1[@class='H1-xsrgru-0 jdfXCo d-sm-inline-block breadcrumb-item active']")
+        ListingQuantity = self.driver.find_elements("xpath", "/html/body/div[@id='__next']/div[@class='UtilityProvider-wqw4uj-0 gDcynq']/div[@class='appprovider__AppThemeWrapper-asxde5-0 gWkYZE']/div[@class='Layout__LayoutStyled-sc-9y7jis-0 ibZBWk page-container']/div[@class='Container-u38a83-0 jDuhNh inner-container container']/div[@class='Row-sc-2hg243-0 iUVxfs align-items-center mb-4 row']/div[@class='Col-sc-14ninbu-0 lfGZKA mb-3 mb-sm-0 col-12 col-md-8']/span[@class='Breadcrumb-sc-15mocrt-0 aAjZo Breadcrumb-sc-1df07y0-0 bLMdQH valing-center d-block breadcrumb']/h1[@class='H1-xsrgru-0 jdfXCo d-sm-inline-block breadcrumb-item active']")
 
-        return int(ListingQuantity.text.split(" ")[0])
+        return len(ListingQuantity)
 
     def GetUrls(self):
-        urls = self.driver.find_elements_by_xpath("//a[@class='sc-bdVaJa ebNrSm']")
+        urls = self.driver.find_elements("xpath", "//a[@class='sc-bdVaJa ebNrSm']")
 
         url_list = []
 
@@ -50,7 +50,7 @@ class ScraperUrls():
 
     def NextPage(self):
         #Footer = self.driver.find_element_by_xpath("/html/body/div[@id='__next']/div[@class='UtilityProvider-wqw4uj-0 gDcynq']/div[@class='appprovider__AppThemeWrapper-asxde5-0 gWkYZE']/div[@class='Layout__LayoutStyled-sc-9y7jis-0 ibZBWk page-container']/div[@class='Container-u38a83-0 jDuhNh inner-container container']/section[@class='Prefooter-sc-9b263-0 igRJXo']/div[@class='Row-sc-2hg243-0 iUVxfs row']/div[@class='Col-sc-14ninbu-0 lfGZKA text-left text-lg-center col-lg-12']")
-        NextPageButton = self.driver.find_element_by_xpath("/html/body/div[@id='__next']/div[@class='UtilityProvider-wqw4uj-0 gDcynq']/div[@class='appprovider__AppThemeWrapper-asxde5-0 gWkYZE']/div[@class='Layout__LayoutStyled-sc-9y7jis-0 ibZBWk page-container']/div[@class='Container-u38a83-0 jDuhNh inner-container container']/div[@class='Row-sc-2hg243-0 iUVxfs row']/div[@class='Col-sc-14ninbu-0 lfGZKA col-md-8 col-lg-9']/ul[@class='sc-dVhcbM kVlaFh Pagination-w2fdu0-0 cxBThU paginator justify-content-center align-items-baseline pagination']/li[@class='item-icon-next page-item']/a[@class='sc-bdVaJa ebNrSm page-link']")
+        NextPageButton = self.driver.find_element("xpath","/html/body/div[@id='__next']/div[@class='UtilityProvider-wqw4uj-0 gDcynq']/div[@class='appprovider__AppThemeWrapper-asxde5-0 gWkYZE']/div[@class='Layout__LayoutStyled-sc-9y7jis-0 ibZBWk page-container']/div[@class='Container-u38a83-0 jDuhNh inner-container container']/div[@class='Row-sc-2hg243-0 iUVxfs row']/div[@class='Col-sc-14ninbu-0 lfGZKA col-md-8 col-lg-9']/ul[@class='sc-dVhcbM kVlaFh Pagination-w2fdu0-0 cxBThU paginator justify-content-center align-items-baseline pagination']/li[@class='item-icon-next page-item']/a[@class='sc-bdVaJa ebNrSm page-link']")
         #NextPageButton = self.driver.find_element_by_class_name("sc-bdVaJa ebNrSm page-link")
         webdriver.ActionChains(self.driver).move_to_element(NextPageButton).click(NextPageButton).perform()
         #webdriver.ActionChains(self.driver).move_to_element(Footer)
@@ -59,7 +59,7 @@ class ScraperUrls():
 
     def AcceptCookies(self):
         try:
-            OkBtn = self.driver.find_element_by_xpath("//a[@class='sc-bdVaJa ebNrSm sc-htoDjs brhAsq Button-bepvgg-0 dqiWxy text-center btn-disclaimer btn btn-secondary']")
+            OkBtn = self.driver.find_element("xpath","//a[@class='sc-bdVaJa ebNrSm sc-htoDjs brhAsq Button-bepvgg-0 dqiWxy text-center btn-disclaimer btn btn-secondary']")
             #webdriver.ActionChains(self.driver).move_to_element(NextPageButton).click(NextPageButton).perform()
             OkBtn.click()
         except:
